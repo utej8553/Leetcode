a6d7b476250de3f5f44558bd3b130304d87ef431
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-06 20:56:27 +0530

    Time: 0 ms (100%), Space: 8.1 MB (57.68%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -3,7 +3,7 @@ char* longestCommonPrefix(char** strs, int strsSize) {
     char* ans = (char*)malloc(strlen(first)+1*sizeof(char));
     int z = 0;
     for(int i = 0; i < strlen(first); i++) {
-        for(int j = 0; j < strsSize; j++) {
+        for(int j = 1; j < strsSize; j++) {
             if(strs[j][i]!=first[i]) {
                 ans[z] = '\0';
                 return ans;

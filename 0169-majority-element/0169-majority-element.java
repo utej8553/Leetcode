ea9affed4ee00eecfacc452bcd0d66edfd0715f3
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-10 17:52:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int max = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[max]<nums[i]) {
+                max = i;
+            }
+        }
+        return nums[max];
+    }
+}

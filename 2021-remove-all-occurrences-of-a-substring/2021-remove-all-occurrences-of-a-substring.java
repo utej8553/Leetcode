972537605a01ff530f1167e4070203919c22035f
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-11 14:32:23 +0530

    Time: 2 ms (60.4%), Space: 42.7 MB (70.72%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -2,15 +2,15 @@ class Solution {
     public String removeOccurrences(String s, String part) {
         
         while(true){
-            if(s.contains(part)){
+            if(!s.contains(part)){
+                break;
+            }else {
                 int idx = s.indexOf(part);
                 int start = idx;
                 int end = idx + part.length();
                 s = s.substring(0, start) + s.substring(end);
             }
-            if(!s.contains(part)){
-                break;
-            }
+            
         }
         return s;
     }

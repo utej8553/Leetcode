04c3842bb36d811205f3d7d30ea506efdb59b054
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-11 23:52:42 +0530

    Time: 1 ms (17.31%), Space: 40.3 MB (67.86%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+      if(num==1||num==0){
+            return true;
+        }
+        int i = 2;
+        long prod;
+        while((prod = (long)i * i) <= num){
+            if(prod==num){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}

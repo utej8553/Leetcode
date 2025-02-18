103007dac1bc451717fe3d64bae3b22fdfe52db6
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-18 19:01:49 +0530

    Time: 5 ms (1.13%), Space: 55.8 MB (93.28%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,7 +1,5 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        return IntStream.range(0, arr.length)
-                .reduce((i, j) -> arr[i] > arr[j] ? i : j)
-                .orElse(-1);
+        return IntStream.range(0, arr.length).reduce((i, j) -> arr[i] > arr[j] ? i : j).orElse(-1);
     }
 }

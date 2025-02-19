9c837882fa40d4dfde467f174616ff2f52cb2514
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-19 19:30:36 +0530

    Time: 1 ms (99.09%), Space: 41.6 MB (79.95%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,25 +1,27 @@
 class Solution {
+    private String res = "";
+    private int perm = 0;
+
     public String getHappyString(int n, int k) {
-        List<String> l = new ArrayList<>();
-        Func(n, 0, l, new StringBuilder(), "abc");
-        if(k>l.size()){
-            return "";
-        } else{
-            return l.get(k-1);
-        }
+        Func(n, 0, k, new StringBuilder(), "abc");
+        return res;
     }
-    public void Func(int size, int index, List<String> l, StringBuilder str, String s){
-        if(index==size) {
-            l.add(str.toString());
+
+    public void Func(int size, int index, int k, StringBuilder str, String s) {
+        if (index == size) {
+            perm++;
+            if (perm == k) {
+                res = str.toString();
+            }
             return;
         }
-        for(int i = 0; i < s.length(); i++){
-            if(str.length()==0||str.charAt(str.length()-1)!=s.charAt(i)){
+        for (int i = 0; i < s.length(); i++) {
+            if (str.length() == 0 || str.charAt(str.length() - 1) != s.charAt(i)) {
                 str.append(s.charAt(i));
-                Func(size, index+1, l, str, s);
+                Func(size, index + 1, k, str, s);
                 str.deleteCharAt(str.length() - 1);
+                if (!res.isEmpty()) return;
             }
         }
-        return;
     }
 }

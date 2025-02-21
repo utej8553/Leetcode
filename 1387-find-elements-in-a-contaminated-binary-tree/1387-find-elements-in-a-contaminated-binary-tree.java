0b39b56dcaa521f2c4c84c9f30c777c08c47d917
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-21 13:47:48 +0530

    Time: 25 ms (30.19%), Space: 47.5 MB (52.83%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -1,42 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class FindElements {
     Set<Integer> set = new HashSet<>();
 
     public FindElements(TreeNode root) {
-        Func(root, 0);
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> values = new Stack<>();
+        stack.push(root);
+        values.push(0);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int value = values.pop();
+            set.add(value);
+
+            if (node.right != null) {
+                stack.push(node.right);
+                values.push(2 * value + 2);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+                values.push(2 * value + 1);
+            }
+        }
     }
-    
+
     public boolean find(int target) {
         return set.contains(target);
     }
-    public void Func(TreeNode root, int value){
-        if(root==null){
-            return;
-        } 
-        root.val = value;
-        set.add(value);
-        Func(root.left, 2*value+1);
-        Func(root.right, 2*value+2);
-        return;
-    }
 }
-
-/**
- * Your FindElements object will be instantiated and called as such:
- * FindElements obj = new FindElements(root);
- * boolean param_1 = obj.find(target);
- */

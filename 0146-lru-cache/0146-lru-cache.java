642cc96ebba6122f936e041b257075d95da4950c
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-02-26 10:17:51 +0530

    Time: 45 ms (71.14%), Space: 111.3 MB (94.01%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,24 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache {
+    private final int capacity;
+    private final LinkedHashMap<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}

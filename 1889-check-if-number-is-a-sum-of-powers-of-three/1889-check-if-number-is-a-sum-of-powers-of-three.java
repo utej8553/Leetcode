154d1e5a39a84d80d106fbf118157991906f94ba
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-03-05 16:14:16 +0530

    Time: 6 ms (10.66%), Space: 40.2 MB (89.05%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,9 +1,21 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-         while (n > 0) {
-            if (n % 3 == 2) return false;
-            n /= 3;
+        return Func(n, 0, 1);
+    }
+
+    public boolean Func(int n, int sum, int powerval) {
+        if (sum > n) {
+            return false;
         }
-        return true;
+        if (sum == n) {
+            return true;
+        }
+        if(powerval>n){
+            return false;
+        }
+        if (Func(n, sum + powerval, powerval * 3)) {
+            return true;
+        }
+        return Func(n, sum, powerval * 3);
     }
 }

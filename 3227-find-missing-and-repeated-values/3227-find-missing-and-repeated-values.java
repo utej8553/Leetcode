ecea125a4e991dfce8fd47153dc202097f333f60
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-03-06 17:51:52 +0530

    Time: 6 ms (24.14%), Space: 45.7 MB (26.65%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i =0; i < grid.length; i++){
+        Set<Integer> set = new HashSet<>();
+        int[] res = new int[2];
+        int n = grid.length;
+        for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[i].length; j++){
-                map.put(grid[i][j], map.getOrDefault(grid[i][j], 0)+1);
+                int num = grid[i][j];
+                if(set.contains(num)){
+                    res[0] = num;
+                }
+                set.add(num);
             }
         }
-        int N = grid.length*grid.length;
-        int[] res = new int[2];
-        for (int i = 1; i <= N; i++) {
-            int count = map.getOrDefault(i, 0);
-            if (count == 2) res[0] = i;
-            if (count == 0) res[1] = i;
+        int sum = 0;
+        int form = (n * n * (n * n + 1)) / 2;
+        for(int num: set){
+            sum = sum + num;
         }
+        res[1] = form-sum;
         return res;
     }
 }

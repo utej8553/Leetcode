4d70055bb5121fd7b0373b970390193ea473075a
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-03-21 23:35:01 +0530

    Time: 377 ms (19.15%), Space: 41.1 MB (72.97%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    public boolean solve(char[][] board){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, i, j, num)) {
+                            board[i][j] = num;
+                            if (solve(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num || 
+                board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

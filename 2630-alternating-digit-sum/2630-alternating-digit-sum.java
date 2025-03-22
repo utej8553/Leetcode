f606bb0671eee5675f86e1173b3eae8535712d46
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-03-22 19:23:29 +0530

    Time: 0 ms (100%), Space: 40.8 MB (25.61%) - LeetHub

---
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        String str = Integer.toString(n);
+        int sum = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(i%2==0){
+                sum = sum + (str.charAt(i)-'0');
+            } else {
+                sum = sum - (str.charAt(i)-'0');
+            }
+        }
+        return sum;
+    }
+}

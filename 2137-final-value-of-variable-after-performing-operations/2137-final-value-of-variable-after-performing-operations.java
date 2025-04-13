d4a5261a5dd7700ae4ec950a4e7b0d954969ed1f
Avulla Utej <91786744+utej8553@users.noreply.github.com>
2025-04-13 12:27:55 +0530

    Time: 1 ms (76.85%), Space: 42.7 MB (60.15%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int finalValueAfterOperations(String[] arr) {
         int res = 0;
         for(int i = 0; i < arr.length; i++){
-            if(arr[i].contains("+")){
+            if(arr[i].equals("X++")||arr[i].equals("++X")){
                 res++;
             } else{
                 res--;

5d2ea0cf31192dbdfea8bad852a8333cf6faef21
Desky <91786744+utej8553@users.noreply.github.com>
2025-11-14 12:08:10 +0530

    Time: 0 ms (100%), Space: 46.8 MB (8.07%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,22 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
+        if (head == null || head.next == null) {
+            return false;
+        }
         ListNode slow = head;
-        while(fast!=null&&fast.next!=null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast==slow){
-                return true;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;  
+            fast = fast.next.next;  
+            
+            if (slow == fast) {
+                return true; 
             }
         }
+        
         return false;
     }
+    
 }

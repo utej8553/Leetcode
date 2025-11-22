38efd51f46557a2bf25de99c3f0295c25c505bea
Desky <91786744+utej8553@users.noreply.github.com>
2025-11-22 17:01:40 +0530

    Time: 0 ms (100%), Space: 43.8 MB (12.35%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,21 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int[][] matrix2 = new int[matrix.length][matrix[0].length];
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                matrix2[i][j] = matrix[j][i];
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length/2; j++) {
-                int temp = matrix2[i][j];
-                matrix2[i][j] = matrix2[i][matrix[i].length - 1 - j];
-                matrix2[i][matrix[i].length - 1 - j] = temp;
-            }
-        }
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                matrix[i][j] = matrix2[i][j];
+        for (int i = 0; i < n; i++) {
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
             }
         }
     }
